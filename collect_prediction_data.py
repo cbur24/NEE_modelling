6a2d21833956ca65ac2105af35aa47d016209ba7
cbur24 <c.a.burton123@gmail.com>
2022-05-09 17:26:31 +1000

    create flux models and predict

--- collect_prediction_data.py
+++ collect_prediction_data.py
@@ -6,95 +6,98 @@ import pandas as pd
 from odc.algo import xr_reproject
 from odc.geo.xr import assign_crs
 
-def collect_prediction_data(time, verbose=True):
-    
-    # Retrieving ANU climate data (1 km resolution)
-    if verbose:
-        print('Collecting data for year: '+time)
-        print('   Extracting ANU Climate data')
-        
-    base='/g/data/gh70/ANUClimate/v2-0/stable/month/'
-    Ta = xr.open_mfdataset([base+'tavg/'+time+'/'+i for i in os.listdir(base+'tavg/'+time+'/')],
-                          chunks=dict(lat=1000, lon=1000)).compute()
-    Ta = assign_crs(Ta, crs='epsg:4283') #GDA94
-    Ta = Ta.drop('crs').tavg
-    Ta = Ta.rename({'lat':'latitude', 'lon':'longitude'})
-    
-#     Tmax = xr.open_mfdataset([base+'tmax/'+time+'/'+i for i in os.listdir(base+'tmax/'+time+'/')]).compute()
-#     Tmax = assign_crs(Tmax, crs='epsg:4283') #GDA94
-#     Tmax = Tmax.drop('crs').tmax
-#     Tmax = Tmax.rename({'lat':'latitude', 'lon':'longitude'})
-    
-#     Tmin = xr.open_mfdataset([base+'tmin/'+time+'/'+i for i in os.listdir(base+'tmin/'+time+'/')]).compute()
-#     Tmin = assign_crs(Tmin, crs='epsg:4283') #GDA94
-#     Tmin = Tmin.drop('crs').tmin
-#     Tmin = Tmin.rename({'lat':'latitude', 'lon':'longitude'})
 
-    # srad = xr.open_mfdataset([base+'srad/'+time+'/'+i for i in os.listdir(base+'srad/'+time+'/')]).compute()
-    # srad = assign_crs(srad, crs='epsg:4283') #GDA94
-    # srad = srad.drop('crs').srad
-    # srad = srad.rename({'lat':'latitude', 'lon':'longitude'})
+def round_coords(ds):
+    """
+    Due to precision of float64 on coordinates, the lai/lst/fpar coordinates
+    don't quite match after reprojection, resulting in adding spurious
+    pixels after merge. Converting to float32 rounds coords so they match.
+    """
+    ds['latitude'] = ds.latitude.astype('float32')
+    ds['longitude'] = ds.longitude.astype('float32')
+    return ds
     
-    precip = xr.open_mfdataset([base+'rain/'+time+'/'+i for i in os.listdir(base+'rain/'+time+'/')],
-                              chunks=dict(lat=1000, lon=1000)).compute()
-    precip = assign_crs(precip, crs='epsg:4283') #GDA94
-    precip = precip.drop('crs').rain
-    precip = precip.rename({'lat':'latitude', 'lon':'longitude'})
-    
-    vpd = xr.open_mfdataset([base+'vpd/'+time+'/'+i for i in os.listdir(base+'vpd/'+time+'/')],
-                           chunks=dict(lat=1000, lon=1000)).compute()
-    vpd = assign_crs(vpd, crs='epsg:4283') #GDA94
-    vpd = vpd.drop('crs').vpd
-    vpd = vpd.rename({'lat':'latitude', 'lon':'longitude'})
 
-    clim = xr.merge([Ta, precip, vpd], compat='override')
+def collect_prediction_data(time_start, time_end, verbose=True):
     
     # Leaf Area Index from MODIS
     if verbose:
-        print('   Loading MODIS LAI')
-    lai = xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/LAI/LAI_500m_monthly_'+time+'.nc').compute()
+        print('   Extracting MODIS LAI')
+    lai = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/LAI_5km_monthly_2002_2021.nc'))
+    lai = lai.sel(time=slice(time_start, time_end))
     
-    ## Soil moisture from GRAFS
+    # LST from MODIS
     if verbose:
-        print('   Loading soil moisture')
-    sws = xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/GRAFS/GRAFS_1km_monthly_'+time+'.nc').compute()
-
-    # Reproject to match ANU Clim 1km grid
-    if verbose:
-        print('   Reprojecting datasets')
-    sws = xr_reproject(sws, geobox=clim.geobox, resampling='bilinear')
-    lai = xr_reproject(lai, geobox=clim.geobox, resampling='average')
+        print('   Extracting MODIS LST')
+    lst = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/LST_5km_monthly_2002_2021.nc'))
+    lst = lst.sel(time=slice(time_start, time_end))
     
-    # Due to precision of Float64 on coordinates, the lai/sws/clim coordinates
-    # don't quite match after reprojection, resulting in adding spurious pixels after merge.
-    # converting to float32 rounds coords so they match
-    clim['latitude'] = clim.latitude.astype('float32')
-    clim['longitude'] = clim.longitude.astype('float32')
+    # fPAR from MODIS
+    if verbose:
+        print('   Extracting MODIS fPAR')
+    fpar = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/FPAR_5km_monthly_2002_2021.nc'))
+    fpar = fpar.sel(time=slice(time_start, time_end))
+    
+    # Delta temp
+    if verbose:
+        print('   Extracting dT')
+    dT = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/LST_Tair_2002_2021.nc'))
+    dT = dT.sel(time=slice(time_start, time_end))
+    
+    # SPEI
+    if verbose:
+        print('   Extracting SPEI')
+    spei = xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/SPEI/chirps_spei_gamma_06.nc')
+    spei = spei.rename({'spei_gamma_06':'spei'})
+    spei = round_coords(spei.rename({'lat':'latitude', 'lon':'longitude'}))
+    spei = spei.sel(time=slice(time_start, time_end))
 
-    sws['latitude'] = sws.latitude.astype('float32')
-    sws['longitude'] = sws.longitude.astype('float32')
+    # Climate
+    if verbose:
+        print('   Extracting Climate')
+    solar = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/AWRA/solar_monthly_wm2_2000_2021.nc'))
+    solar = solar.rename({'solar_exposure_day':'solar'})
+    solar = solar.sel(time=slice(time_start, time_end))
+    
+    tavg = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/AWRA/tavg_monthly_1991_2021.nc'))
+    tavg = tavg.rename({'temp_avg_month':'Ta'})
+    tavg = tavg.sel(time=slice(time_start, time_end))
+    
+    vpd = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/AWRA/vpd_monthly_2000_2021.nc'))
+    vpd = vpd.sel(time=slice(time_start, time_end))
 
-    lai['latitude'] = lai.latitude.astype('float32')
-    lai['longitude'] = lai.longitude.astype('float32')
+    rain = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/chirps_aus_monthly_1991_2021.nc'))
+    rain = rain.sel(time=slice(time_start, time_end))
+
+    #add lags to rainfall
+    if verbose:
+        print('   Adding Rainfall lags')
+    rain_l1 = rain.shift(time=1).rename({'precip':'precip_L1'})
+    rain_l2 = rain.shift(time=2).rename({'precip':'precip_L2'})
+    rain_l3 = rain.shift(time=3).rename({'precip':'precip_L3'})
+    rain = xr.merge([rain,rain_l1,rain_l2,rain_l3])
+    
+    # landcover
+    if verbose:
+        print('   Adding Landcover class')
+    lc = round_coords(xr.open_dataset('/g/data/os22/chad_tmp/NEE_modelling/data/IGBP_Landcover_MODIS_5km.nc'))
+    lc = lc.sel(time=slice(time_start, time_end))
     
     #merge all datasets together
-    data = xr.merge([clim, lai, sws], compat='override')
-    data = data.unify_chunks()
+    if verbose:
+        print('   Merge and create valid data mask')
+    data = xr.merge([lai,lst,fpar,dT,spei,solar,tavg,vpd,rain,lc], compat='override')
     
     #create mask where data is valid (spurios values from reproject)
-    mask = ~np.isnan(data.tavg.isel(time=0))
+    mask = ~np.isnan(data.precip.isel(time=0))
     data = data.where(mask)
-    
-    #add a 1-month lag
-    if verbose:
-        print('   Adding a lag')
-    data_lag1 = data.shift(time=1)
-    for i in data.data_vars:
-        data_lag1 = data_lag1.rename({i:i+'_L1'})
-    data = data.merge(data_lag1)
     
     if verbose:
         print('   Exporting netcdf')
     # export data
-    data = data.rename({'latitude':'y', 'longitude':'x'})
-    data.to_netcdf('/g/data/os22/chad_tmp/NEE_modelling/results/prediction_data/prediction_data_'+time+'.nc')
+    data = data.rename({'latitude':'y', 'longitude':'x'}) #this helps with predict_xr
+    data = data.astype('float32') #make sure all data is in float32
+    
+    data.to_netcdf('/g/data/os22/chad_tmp/NEE_modelling/results/prediction_data/prediction_data_'+time_start+'_'+time_end+'.nc')
+    
+    return data
